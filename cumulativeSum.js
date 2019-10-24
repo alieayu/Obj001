84f8de9a3016c8d15408275456ef31342455f5ce
alieayu <43448888+alieayu@users.noreply.github.com>
2019-10-23 23:43:22 -0700

    Add files via upload

---
+++ cumulativeSum.js
@@ -0,0 +1,40 @@
+const testVar = 41;
+console.log('test varaible is set to');
+console.log(testVar);
+function isPrime(value) {
+  for (let i = 2; i < value; i++) {
+    if (value % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function findPrime(num) {
+  const list = [];
+  let count = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i) === true) {
+      list[count] = i;
+      count++;
+    }
+  }
+  return list;
+}
+console.log('Function 1:');
+const var2 = findPrime(testVar);
+console.log(var2);
+
+function cumulativeSum(list1) {
+  let prev = 0;
+  let count = 0;
+  const list2 = [];
+  for (let i = 0; i < list1.length; i++) {
+    list2[count] = prev + list1[i];
+    prev = list2[count]; count++;
+  }
+  return list2;
+}
+const var3 = cumulativeSum(var2);
+console.log('Function 2:');
+console.log(var3);
